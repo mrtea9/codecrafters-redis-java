adbe58f16803667040cc02ab3d5d64f65e84da9a
mrtea9 <ionmeriacri111@gmail.com>
2024-11-04 15:22:38 +0200

    set and get

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -13,9 +13,11 @@ public class Parser {
     }
 
     public void parse() {
-        String[] sourceList = Arrays.copyOf(this.source.split("\r\n"),this.source.split("\r\n").length - 1) ;
-        for (String element : sourceList) {
-            System.out.println("element = " + element);
+        String[] intermediate = this.source.split("\r\n");
+        String[] sourceList = Arrays.copyOf(intermediate, intermediate.length - 1) ;
+        for (int i = 0; i < sourceList.length; i++) {
+            String element = sourceList[i];
+            System.out.println("element = " + element + ", index = " + i);
         }
         System.out.println();
         System.out.println("sourceList = " + Arrays.toString(sourceList));
