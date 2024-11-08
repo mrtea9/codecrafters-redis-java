02cff8eec71f7848337ae58c568bca66c016650c
mrtea9 <ionmeriacri111@gmail.com>
2024-11-08 15:20:20 +0200

    Replication

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,15 @@ public class Main {
     public static void main(String[] args) {
         EventLoop eventLoop;
 
-        if (args.length > 3) {
+        if (args.length > 3 && !args[0].equals("--port")) {
             String dirName = args[1];
             String dbFileName = args[3];
 
             eventLoop = new EventLoop(dirName, dbFileName);
         } else if (args.length > 1) {
             int port = Integer.parseInt(args[1]);
+            String replicaOf = args[3];
+            System.out.println(replicaOf);
 
             eventLoop = new EventLoop(port);
         } else {
