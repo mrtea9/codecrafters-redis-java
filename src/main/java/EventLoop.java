33ef3bc021ba36ce3504848a38f30afdda29aff5
mrtea9 <ionmeriacri111@gmail.com>
2024-11-11 13:34:20 +0200

    Handshake

--- src/main/java/EventLoop.java
+++ src/main/java/EventLoop.java
@@ -7,6 +7,7 @@ import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -197,6 +198,6 @@ public class EventLoop {
 
         byte[] responseBytes = new byte[bytesRead];
         buffer.get(responseBytes);
-        System.out.println("Received response from master: " + new String(responseBytes));
+        System.out.println("Received response from master: " + new String(responseBytes, StandardCharsets.US_ASCII));
     }
 }
