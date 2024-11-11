080c99f4e7534c6c5a93e1614502dd93bd45bde5
mrtea9 <ionmeriacri111@gmail.com>
2024-11-11 13:37:36 +0200

    Handshake

--- src/main/java/EventLoop.java
+++ src/main/java/EventLoop.java
@@ -198,6 +198,6 @@ public class EventLoop {
 
         byte[] responseBytes = new byte[bytesRead];
         buffer.get(responseBytes);
-        System.out.println("Received response from master: " + new String(responseBytes, StandardCharsets.US_ASCII));
+        System.out.println("Received response from master: " + new String(responseBytes));
     }
 }
