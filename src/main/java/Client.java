06998d962cc37d39a4d36c56b7dd5fbac43bab34
mrtea9 <ionmeriacri111@gmail.com>
2024-11-11 15:20:34 +0200

    RDB Transfer

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -163,14 +163,11 @@ public class Client {
 
         this.channel.write(ByteBuffer.wrap(response.getBytes()));
 
-        createRdbContent();
-
-        //this.channel.write(ByteBuffer.wrap(rdbFile.getBytes()));
+        sendRdbFile();
     }
 
-    private void createRdbContent() throws IOException {
+    private void sendRdbFile() throws IOException {
         String content = "524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2";
-        String binContent = new BigInteger(content, 16).toString(2);
 
         byte[] contents = HexFormat.of().parseHex(content);
 
