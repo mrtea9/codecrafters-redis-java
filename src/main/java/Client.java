b4d210a6a40fc6b0a9b58e62500a13154b9c6161
mrtea9 <ionmeriacri111@gmail.com>
2024-11-19 12:47:53 +0200

    replication wait

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -43,7 +43,7 @@ public class Client {
 
     private void processResponse(List<String> decodedList) throws IOException {
         String command = decodedList.get(0);
-        System.out.println("client = " + decodedList);
+        System.out.println("client = " + this.channel.getRemoteAddress() + " " + decodedList);
         if (command.equalsIgnoreCase("ping")) {
             processPing();
         } else if (command.equalsIgnoreCase("echo")) {
