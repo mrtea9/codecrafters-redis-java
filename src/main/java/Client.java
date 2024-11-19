71e628b175cc3dbf1d817d13aa0657a5d0f0f56a
mrtea9 <ionmeriacri111@gmail.com>
2024-11-19 12:42:11 +0200

    replication wait

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -113,7 +113,7 @@ public class Client {
         this.eventLoop.propagateCommand("SET", key, value);
 
         try {
-            Thread.sleep(1000);
+            Thread.sleep(1200);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
