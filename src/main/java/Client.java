6947fadb09926e86b4237442eec6ee58aaf8db2e
mrtea9 <ionmeriacri111@gmail.com>
2024-11-26 16:36:10 +0200

    streams

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -270,7 +270,6 @@ public class Client {
 
                 if (!processing && startRange.equals("$")) {
                     processing = true;
-                    System.out.println("Da");
                     continue;
                 }
 
